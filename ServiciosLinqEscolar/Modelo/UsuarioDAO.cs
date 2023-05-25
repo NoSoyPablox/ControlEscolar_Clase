19155808724f9156df88a5fad37f298e89239dd2
NoSoyPablox <PabloHernan298@gmail.com>
2023-05-25 16:50:13 -0600

    Agregar archivos de proyecto.

---
+++ ServiciosLinqEscolar/Modelo/UsuarioDAO.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace ServiciosLinqEscolar.Modelo
+{
+    public class UsuarioDAO
+    {
+        public static List<usuario> obtenerUsuarios()
+        {
+            DataClassesEscolarUVDataContext conexionBD = getConnection();
+            //var usuarios = asdasdasd <--- no tiene tipo hasta que se le asigna algo asdasd
+            IQueryable<usuario> usuariosBD = from usuarioQuery in conexionBD.usuario
+                                            select usuarioQuery;
+
+            return usuariosBD.ToList();
+
+        }
+        public static usuario iniciarSesion(string username, string password)
+        {
+            DataClassesEscolarUVDataContext conexionBD = new DataClassesEscolarUVDataContext(global::System.Configuration.ConfigurationManager.ConnectionStrings["escolaruvConnectionString"].ConnectionString);
+
+            var usuario = (from userLogin in conexionBD.usuario
+                           where userLogin.username == username &&
+                           userLogin.password == password
+                           select userLogin).FirstOrDefault();
+            return usuario;
+        }
+
+        public static Boolean guardarUsuario(usuario usuarioNuevo)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+                var usuario = new usuario()
+                {
+                    nombre = usuarioNuevo.nombre,
+                    apellidoPaterno = usuarioNuevo.apellidoPaterno,
+                    apellidoMaterno = usuarioNuevo.apellidoMaterno,
+                    username = usuarioNuevo.username,
+                    password = usuarioNuevo.password
+                };//Permite construir con las propiedades que mandes, independiente al numero de argumentos que tenga el constructor
+                conexionBD.usuario.InsertOnSubmit(usuario);
+                conexionBD.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static Boolean editarUsuario(usuario UsuarioEdicion)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+
+                var usuario = (from usuarioEdicion in conexionBD.usuario
+                               where usuarioEdicion.idUsuario == UsuarioEdicion.idUsuario
+                               select usuarioEdicion).FirstOrDefault();
+                if (usuario != null)
+                {
+                    usuario.nombre = UsuarioEdicion.nombre;
+                    usuario.apellidoPaterno = UsuarioEdicion.apellidoPaterno;
+                    usuario.apellidoMaterno = UsuarioEdicion.apellidoMaterno;
+                    usuario.password = UsuarioEdicion.password;
+
+                    conexionBD.SubmitChanges();
+                    return true;
+                }
+                else { return false; }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool eliminarUsuario(int idUsuario)
+        {
+            try
+            {
+                DataClassesEscolarUVDataContext conexionBD = getConnection();
+                usuario usuarioEliminar = (from usuario in conexionBD.usuario
+                                           where usuario.idUsuario == idUsuario
+                                           select usuario).FirstOrDefault();
+
+                if (usuarioEliminar!= null)
+                {
+                    conexionBD.usuario.DeleteOnSubmit(usuarioEliminar);
+                    conexionBD.SubmitChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static DataClassesEscolarUVDataContext getConnection()
+        {
+            return new DataClassesEscolarUVDataContext(global::System.Configuration.
+                ConfigurationManager.ConnectionStrings["escolaruvConnectionString"].ConnectionString);
+
+        }
+    }
+}
---
+++ ServiciosLinqEscolar/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using ServiciosLinqEscolar.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiciosLinqEscolar
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public List<usuario> ObtenerUsuarios()
+        {
+            List<usuario> usuarioBD = UsuarioDAO.obtenerUsuarios();
+            return usuarioBD;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public bool GuardarUsuario(usuario usuarioRegistro)
+        {
+            return UsuarioDAO.guardarUsuario(usuarioRegistro);
+        }
+
+        public usuario IniciarSesion(string username, string password)
+        {
+            return UsuarioDAO.iniciarSesion(username, password);
+        }
+
+        public bool EditarUsuario(usuario usuarioEdicion)
+        {
+            return UsuarioDAO.editarUsuario(usuarioEdicion);
+        }
+
+        public bool EliminarUsuario(int idUsuario)
+        {
+            return UsuarioDAO.eliminarUsuario(idUsuario);
+        }
+    }
+}
