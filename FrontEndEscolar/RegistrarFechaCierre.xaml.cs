582ac2f6a5198ec93a3df25e3338aeae826b907f
NoSoyPablox <PabloHernan298@gmail.com>
2023-06-09 14:26:02 -0600

    Interfaz registrar fechas de cierre borrador

---
+++ FrontEndEscolar/RegistrarFechaCierre.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FrontEndEscolar
+{
+    /// <summary>
+    /// Lógica de interacción para RegistrarFechaCierre.xaml
+    /// </summary>
+    public partial class RegistrarFechaCierre : Window
+    { 
+        PantallaPrincipal pantallaAnterior = new PantallaPrincipal();
+        public RegistrarFechaCierre()
+        {
+            InitializeComponent();
+        }
+
+        public void recibirVentanaAnterior(PantallaPrincipal pantallaRecibida)
+        {
+            pantallaAnterior = pantallaRecibida;
+        }
+
+        private void btnVolver_Click(object sender, RoutedEventArgs e)
+        {
+            pantallaAnterior.IsEnabled = true;
+            this.Close();
+        }
+    }
+}
